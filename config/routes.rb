9d6e3879026a54d3e8722cdca149651d4a167666
Borja Martín Fernández <bmartinf@axpe.com>
2015-10-14 11:06:14 +0200

    Create opportunities scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  root 'opportunities#index'
+  resources :opportunities
+  
   get 'hello/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
