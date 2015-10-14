ddf49153ee0f9b14ffbde5544ca0d91845c8f86f
Borja Martín Fernández <bmartinf@axpe.com>
2015-10-14 15:54:19 +0200

    Try 2 for Stages

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -1,3 +1,3 @@
 class Opportunity < ActiveRecord::Base
-  self.table_name = 'salesforcebearer.opportunity'
+  self.table_name = 'salesforcebearer.opportunity' unless ENV["RAILS_ENV"] == "development"
 end
